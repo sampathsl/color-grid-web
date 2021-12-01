5d43b6c4536e1a896c3bde1a3f78e1c718c77f6e
Sampath Thennakoon <sampathde4@gmail.com>
2021-12-01 19:57:27 +0530

    Add initial commit

---
+++ src/main/java/io/sampathsl/colorgrid/colorgridweb/ColorGridWebApplication.java
@@ -0,0 +1,13 @@
+package io.sampathsl.colorgrid.colorgridweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorGridWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorGridWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sampathsl/colorgrid/colorgridweb/ColorGridWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sampathsl.colorgrid.colorgridweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorGridWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
